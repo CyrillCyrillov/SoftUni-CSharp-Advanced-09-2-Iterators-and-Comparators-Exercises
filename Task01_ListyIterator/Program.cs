156f53a1fb29c7cbe5d9385714a42ae9cba7621e
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-16 23:25:58 +0200

    Add files via upload

---
+++ Task01_ListyIterator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ListyIterator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {       
+
+            
+            string[] createData = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Skip(1).ToArray();
+
+
+            ListyIterator<string> dataList = new ListyIterator<string>(createData);
+
+            string nextComand;
+            while ((nextComand = Console.ReadLine()) != "END" )
+            {
+                switch (nextComand.ToUpper())
+                {
+                    case "MOVE":
+                        
+                        Console.WriteLine(dataList.Move());
+                        break;
+
+                    case "PRINT":
+
+                        dataList.Print();
+                        break;
+
+                    case "HASNEXT":
+
+                        Console.WriteLine(dataList.HasNext());
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+            
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
