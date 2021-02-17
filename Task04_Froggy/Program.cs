cb159c907c30f7e99d1c5e039088008acc9fc4c6
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-17 23:49:36 +0200

    Add files via upload

---
+++ Task04_Froggy/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+namespace Task03_Stack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            personalStack<int> datas = new personalStack<int>(Console.ReadLine().Split(new string[] { " ", ", " }, StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(int.Parse).ToList());
+
+            bool isExit = false;
+
+            while (true)
+            {
+                string[] comand = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                        // exit
+                if (comand[0].ToUpper() == "END") break;
+                // exit
+
+
+                //comand[0] -> type
+
+                
+                switch (comand[0].ToUpper())
+                {
+                    case "PUSH":
+                        // elementToPush -> int.Parse(comand[1]);
+                        datas.Push(int.Parse(comand[1]));
+                        break;
+
+                    case "POP":
+                        if (datas.NumbersOfElements() == 0)
+                        {
+                            Console.WriteLine("No elements");
+                            isExit = true;
+                        }
+                        else
+                        {
+                            datas.Pop();
+                        }  
+                        break;
+
+                    default:
+                        break;
+                }
+
+            }
+
+            
+            if(datas.NumbersOfElements() > 0)
+            {
+                for (int i = 1; i <= 2; i++)
+                {
+                    foreach (var item in datas)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+            }
+            else if(!isExit)
+            {
+                Console.WriteLine("No elements");
+            }
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Task04_Froggy/personalStack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task03_Stack
+{
+    public class personalStack<T> : IEnumerable<T>
+    {
+        private List<T> elements;
+
+        public personalStack()
+        {
+            elements = new List<T>();
+        }
+
+        public personalStack(List<T> elements)
+        {
+            this.elements = elements;
+        }
+
+        public void Push(T element)
+        {
+            this.elements.Add(element);
+        }
+
+        public T Pop()
+        {
+            int lastIndex = elements.Count - 1;
+            T removedElement = elements[lastIndex];
+            this.elements.RemoveAt(lastIndex);
+            return removedElement;
+        }
+
+        public int NumbersOfElements()
+        {
+            return elements.Count;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = elements.Count - 1; i >= 0; i--)
+            {
+                yield return elements[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
