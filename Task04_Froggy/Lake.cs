1eb339babfda2ebcdf4d7a8a1cbcb2b0c51aa763
Kiril Kirilov <kirilkirilov@abv.bg>
2021-02-18 01:35:43 +0200

    Add files via upload

---
+++ Task04_Froggy/Lake.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task04_Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        private List<int> stones;
+        
+
+        public Lake()
+        {
+            stones = new List<int>();
+        }
+
+        public Lake(List<int> stones)
+        {
+            this.stones = stones;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i <= stones.Count - 1; i += 2)
+            {
+                yield return stones[i];
+            }
+
+            int helpVar = -1;
+            if (stones.Count % 2 == 0) helpVar = stones.Count - 1;
+            else helpVar = stones.Count - 2;
+
+
+            for (int i = helpVar; i >= 0; i -= 2)
+            {
+                yield return stones[i];
+            }
+
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
--- Task04_Froggy/Program.cs
+++ Task04_Froggy/Program.cs
@@ -1,68 +1,19 @@
 ﻿using System;
 using System.Linq;
 
-namespace Task03_Stack
+namespace Task04_Froggy
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            personalStack<int> datas = new personalStack<int>(Console.ReadLine().Split(new string[] { " ", ", " }, StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(int.Parse).ToList());
-
-            bool isExit = false;
-
-            while (true)
-            {
-                string[] comand = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
-
-                        // exit
-                if (comand[0].ToUpper() == "END") break;
-                // exit
-
-
-                //comand[0] -> type
-
-                
-                switch (comand[0].ToUpper())
-                {
-                    case "PUSH":
-                        // elementToPush -> int.Parse(comand[1]);
-                        datas.Push(int.Parse(comand[1]));
-                        break;
-
-                    case "POP":
-                        if (datas.NumbersOfElements() == 0)
-                        {
-                            Console.WriteLine("No elements");
-                            isExit = true;
-                        }
-                        else
-                        {
-                            datas.Pop();
-                        }  
-                        break;
-
-                    default:
-                        break;
-                }
-
-            }
-
             
-            if(datas.NumbersOfElements() > 0)
-            {
-                for (int i = 1; i <= 2; i++)
-                {
-                    foreach (var item in datas)
-                    {
-                        Console.WriteLine(item);
-                    }
-                }
-            }
-            else if(!isExit)
-            {
-                Console.WriteLine("No elements");
-            }
+
+            Lake frogWay = new Lake(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList());
+
+            Console.WriteLine(string.Join(", ", frogWay));
+            
+            
             //Console.WriteLine("Hello World!");
         }
     }
